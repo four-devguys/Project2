47aeee93073ca239b1bcfdd237c3adac8fe2121f
HB <hbianan@gmail.com>
2019-08-20 14:18:48 -0400

    files updated

--- routes/html-api-routes.js
+++ routes/html-api-routes.js
@@ -44,7 +44,7 @@ var data = {
     title: "Emotion Tracker"
 };
 
-  res.render("index",data)
+  res.render("login",data)
   });
 });
 }
@@ -79,7 +79,7 @@ function findUserEmoji(){
     // If the user already has an account send them to the members page
     if (req.user) {
       res.redirect("/members");
-    }
+    }    
     res.sendFile(path.join(__dirname, "../public/signup.html"));
   });
 
